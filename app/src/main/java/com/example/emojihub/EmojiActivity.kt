b4843c6645e7ff7e7bb517489e870127ff0ec918
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-07-09 00:07:34 +0530

    created setupEmojiRecyclerView() to setup recycler view.

--- app/src/main/java/com/example/emojihub/EmojiActivity.kt
+++ app/src/main/java/com/example/emojihub/EmojiActivity.kt
@@ -2,10 +2,23 @@ package com.example.emojihub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.emojiAdapter.EmojiAdapter
 
 class EmojiActivity : AppCompatActivity() {
+    private val emojiRecyclerView: RecyclerView
+        get() = findViewById(R.id.emoji_rv)
+    private lateinit var emojiAdapter: EmojiAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.emoji_activity)
+        setupEmojiRecyclerView()
+    }
+
+    private fun setupEmojiRecyclerView() {
+        emojiRecyclerView.layoutManager = LinearLayoutManager(this@EmojiActivity)
+        emojiAdapter = EmojiAdapter()
+        emojiRecyclerView.adapter = emojiAdapter
     }
 }
