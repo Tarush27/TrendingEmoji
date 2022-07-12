66cd4ba419e22b75cf9911dd79b8d7d351fccc37
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-07-12 17:43:06 +0530

    created emojiModels of the type Array List to show emoji's in recycler view. implemented on bind view holder function to bind view holder with emoji data set via adapter. modified getItemCount fun to return size of the emojiModels.

--- app/src/main/java/com/example/emojiAdapter/EmojiAdapter.kt
+++ app/src/main/java/com/example/emojiAdapter/EmojiAdapter.kt
@@ -3,10 +3,13 @@ package com.example.emojiAdapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.emojiDataSet.EmojiModel
 import com.example.emojiViewHolder.EmojiViewHolder
 import com.example.emojihub.R
 
 class EmojiAdapter : RecyclerView.Adapter<EmojiViewHolder>() {
+
+    private val emojiModels: ArrayList<EmojiModel> = arrayListOf()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmojiViewHolder {
         val emojiViewHolder =
             LayoutInflater.from(parent.context).inflate(R.layout.single_emoji, parent, false)
@@ -14,10 +17,11 @@ class EmojiAdapter : RecyclerView.Adapter<EmojiViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: EmojiViewHolder, position: Int) {
-
+        val emojiModel = emojiModels[position]
+        holder.emojiTextView.text = emojiModel.unicodeCharacter
     }
 
     override fun getItemCount(): Int {
-        return 5
+        return emojiModels.size
     }
 }
