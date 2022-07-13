dabe954581bae347975a8c9f7c2b4a005a86fc24
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-07-13 23:31:27 +0530

    created a EmojiResponseFile data class to store the parsed json responses.

---
+++ app/src/main/java/com/example/network/EmojiResponseFile.kt
@@ -0,0 +1,8 @@
+package com.example.network
+
+import com.google.gson.annotations.SerializedName
+
+data class EmojiResponseFile(
+    @SerializedName("unicode") val unicode: ArrayList<String> = arrayListOf()
+) {
+}
