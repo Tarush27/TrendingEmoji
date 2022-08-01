0a416d4eaf2a86c718f23487b7abcc7a880c407b
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-08-01 14:27:11 +0530

    inside get annotation replaced "/random" endpoint with "/all".

--- app/src/main/java/com/example/network/RetrofitApiService.kt
+++ app/src/main/java/com/example/network/RetrofitApiService.kt
@@ -1,9 +1,8 @@
 package com.example.network
 
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface RetrofitApiService {
-    @GET("/random")
-    fun getEmoji(): Call<EmojiResponseFile>
+    @GET("/all")
+    fun getEmoji(): retrofit2.Call<EmojiResponseFile>
 }
